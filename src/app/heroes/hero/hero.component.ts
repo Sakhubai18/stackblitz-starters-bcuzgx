215b4c9f0cde0b9a288c5c7f2a4891c9e828a2c4
Sakhubai18 <134392879+Sakhubai18@users.noreply.github.com>
2023-08-12 13:51:06 +0530

    3 files modified

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../../hero';
 
 @Component({
-  standalone:true,
+  standalone: true,
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent implements OnInit {
+  @Input() hero?: Hero;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.hero);
   }
-
 }
